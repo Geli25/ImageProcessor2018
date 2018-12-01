a93709b0c14b6cfaf607ad1e3c341766b3a82cc2
Angelina <h1255513@gmail.com>
2018-11-30 23:26:53 -0500

    set up router page and menu bar

--- image-processing-gui/src/App.js
+++ image-processing-gui/src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'; 
-import {} from 'react-router-dom';
-import Input from './Input/Stateful/Input';
+import {Route, withRouter, Switch} from 'react-router-dom';
+import NavBar from './Navigation/NavBarWrapper';
+import Input from './Input/Input';
 import './App.css';
 
 class App extends Component {
     render() {
+        let allRoutes = (
+            <Switch>
+                <Route path="/" exact component={Input} />
+            </Switch>
+        );
+
         return (
-            <div className="App">
-            <Input />
+            <div className="app">
+                <NavBar>
+                    {allRoutes}
+                </NavBar>
             </div>
         );
     }
 }
 
-export default App;
+export default withRouter(App);
