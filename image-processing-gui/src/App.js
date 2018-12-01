30a6875ded0d4580a0ac1d1521b6a98cf120097f
Angelina <h1255513@gmail.com>
2018-12-01 00:39:55 -0500

    added Output placeholder&router

--- image-processing-gui/src/App.js
+++ image-processing-gui/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Route, withRouter, Switch} from 'react-router-dom';
 import NavBar from './Navigation/NavBarWrapper';
 import Input from './Input/Input';
+import Output from './Output/Output';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,8 @@ class App extends Component {
         let allRoutes = (
             <Switch>
                 <Route path="/" exact component={Input} />
+                {/* for passing props */}
+                <Route path="/results" exact component={()=><Output />} />
             </Switch>
         );
 
