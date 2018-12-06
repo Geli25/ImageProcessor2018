a44fd902d5dbc43c1b57353aed1b84dec0a8cfa3
Angelina <h1255513@gmail.com>
2018-12-06 00:32:53 -0500

    added axios post and tested on local server

--- image-processing-gui/src/Input/Input.js
+++ image-processing-gui/src/Input/Input.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
-import {Line} from 'rc-progress';
+import Loader from './../UI/Loader';
 import {Redirect} from 'react-router-dom';
 import Welcome from './Stateless/Welcome';
 import Instructions from './Stateless/Instructions';
@@ -11,8 +11,6 @@ import Instruction2 from './Stateless/Instruction2';
 class Input extends Component {
     state = {
         loading:false,
-        percentage:null,
-        sent:false,
         fileNames:[],
         jsonData:{
             "files":[],
@@ -73,7 +71,26 @@ class Input extends Component {
     }
 
     submitButton=()=>{
+        // if (this.props.sentStatus===true){
+        //     let newData = {
+        //         "uuid": this.props.uuid,
+        //         "HE": this.state.jsonData.HE,
+        //         "CS": this.state.jsonData.CS,
+        //         "LC": this.state.jsonData.LC,
+        //         "RV": this.state.jsonData.RV,
+        //     }
+        //     this.setState({
+        //         loading: true},
+        //     ()=>console.log(newData))
+        // }
+        // else{
+        //     this.setState({ loading: true, sent: true }, () => {
+        //         console.log(this.state);
+        //         this.props.keepFilenames(this.state.fileNames);
+        //     })
+        // }
         if (this.props.sentStatus===true){
+            this.setState({ loading: true });
             let newData = {
                 "uuid": this.props.uuid,
                 "HE": this.state.jsonData.HE,
@@ -81,30 +98,38 @@ class Input extends Component {
                 "LC": this.state.jsonData.LC,
                 "RV": this.state.jsonData.RV,
             }
-            this.setState({
-                loading: true},
-            ()=>console.log(newData))
-        }
-        else{
-            this.setState({ loading: true, sent: true }, () => {
-                console.log(this.state);
-                this.props.keepFilenames(this.state.fileNames);
+            axios.post('http://127.0.0.1:5000/new_data', newData, {
+                onUploadProgress: progressEvent => {
+                    let progressPercent = (progressEvent.loaded / progressEvent.total);
+                    console.log(progressPercent);
+                }
+            }).then(response => {
+                this.setState({ loading: false }, () => {
+                    console.log(newData, response);
+                    this.props.keepFilenames(this.state.fileNames);
+                });
+            }).catch(err => {
+                this.setState({ loading: false });
+                alert("Something went wrong, please try again. Error: " + err);
             })
         }
-        // axios.post('http://vcm-7506.vm.duke.edu:5000/new_user',this.state.jsonData,{
-        //     onUploadProgress: progressEvent => {
-        //         let progressPercent = (progressEvent.loaded / progressEvent.total)*100;
-        //         this.setState({percentage:progressPercent},()=>{
-        //             console.log(progressPercent);
-        //         });
-        //     }
-        // }).then(response=>{
-        //     this.setState({loading:false});
-        //     console.log(response);
-        // }).catch(err=>{
-        //     this.setState({loading:false});
-        //     alert("Something went wrong, please try again. Error: "+err);
-        // })
+        else{
+            this.setState({ loading: true });
+            axios.post('http://127.0.0.1:5000/new_user', this.state.jsonData, {
+                onUploadProgress: progressEvent => {
+                    let progressPercent = (progressEvent.loaded / progressEvent.total);
+                    console.log(progressPercent);
+                }
+            }).then(response => {
+                this.setState({ loading: false },()=>{
+                    console.log(this.state.jsonData, response);
+                    this.props.keepFilenames(this.state.fileNames);
+                });
+            }).catch(err => {
+                this.setState({ loading: false });
+                alert("Something went wrong, please try again. Error: " + err);
+            })
+        }
     }
 
 
@@ -125,7 +150,7 @@ class Input extends Component {
 
                     toggle={this.optionToggle}
                     optionData={this.state.jsonData} />
-                {this.state.loading ? <Line style={{height:'20px'}} percent={this.state.percentage} strokeWidth="6" strokeColor="black" /> : <button disabled={disable} onClick={this.submitButton}>Submit</button>}
+                {this.state.loading ? <Loader /> : <button disabled={disable} onClick={this.submitButton}>Submit</button>}
             </Fragment>
         );
         
