0601dba575b7875844f3a7a248c2e090409f7b2b
HanxinHua <HanxinHua@github.com>
2018-12-09 16:44:32 -0500

    validation done

---
+++ test_validation.py
@@ -0,0 +1,87 @@
+import pytest
+from validation import *
+
+
+@pytest.mark.parametrize("file_name, expected", [
+    ("test_resource/unzip/kk.zip", True),
+    ("test_resource/unzip/pic.zip", True),
+    ])
+def test_un_zip(file_name, expected):
+    dir_name = un_zip(file_name)
+    response = os.path.isdir(dir_name)
+    assert response == expected
+
+
+@pytest.mark.parametrize("image_file, image_type", [
+    ("test_resource/turn_grey/1.jpg", "JPG"),
+    ("test_resource/turn_grey/2.jpg", "JPG"),
+    ("test_resource/turn_grey/3.jpg", "JPG"),
+    ])
+def test_image_turn_grey(image_file, image_type):
+    encoded = image_turn_grey(image_file, image_type)
+    image_bytes = base64.b64decode(encoded)
+    decoded = io.BytesIO(image_bytes)
+    img = Image.open(decoded).convert('RGB')
+    w, h = img.size
+    for i in range(w):
+        for j in range(h):
+            r, g, b = img.getpixel((i, j))
+            assert r == g == b
+
+
+@pytest.mark.parametrize("path, my_data, name, expected", [
+    ("test_resource/traverse/test1", [[], [], []], "hh", 7),
+    ("test_resource/traverse/test2", [[], [], []], "angelina", 6),
+    ("test_resource/traverse/test3", [[], [], []], "Tina", 8),
+    ])
+def test_traverse_dir(path, my_data, name, expected):
+    traverse_dir(path, my_data, name)
+    assert expected == len(my_data[0])
+
+
+def read_file_as_b64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read())
+
+
+@pytest.mark.parametrize("database, expected", [
+    ({"CS": True,
+      "HE": False,
+      "LC": False,
+      "RV": False,
+      "uuid": "1",
+      "files": [read_file_as_b64("test_resource/validate/pic.zip"),
+                read_file_as_b64("test_resource/validate/holk.jpg"),
+                read_file_as_b64("test_resource/validate/ironman.jpg")],
+      "fileNames": ["pic.zip", "holk.jpg", "ironman.jpg"]
+      }, 4)
+    ])
+def test_validate(database, expected):
+    response = validate(database)
+    assert expected == len(response[0])
+
+
+@pytest.mark.parametrize("new_database, file_names, expected", [
+    ({"CS": True,
+      "HE": False,
+      "LC": False,
+      "RV": False,
+      "uuid": 1,
+      "selectedFilename": ["1.jpg", "hh.zip/2.jpg"]
+      },
+     ["1.jpg", "2.jpg", "3.jpg", "tina.zip/4.jpg", "hh.zip/2.jpg", "angelina.zip/7.jpg"],
+     [0, 4]
+     ),
+    ({"CS": True,
+      "HE": False,
+      "LC": False,
+      "RV": False,
+      "uuid": 1,
+      "selectedFilename": ["2.jpg", "angelina.zip/7.jpg"]
+      },
+     ["1.jpg", "2.jpg", "3.jpg", "tina.zip/4.jpg", "hh.zip/2.jpg", "angelina.zip/7.jpg"],
+     [1, 5])
+])
+def test_second_validation(new_database, file_names, expected):
+    response = second_validation(new_database, file_names)
+    assert response[0] == expected
---
+++ validation.py
@@ -0,0 +1,120 @@
+import base64
+import zipfile
+import os
+import datetime as dt
+import logging
+from PIL import Image
+import io
+import shutil
+
+all_type = ["JPEG", "JPG", "TIFF", "PNG"]
+
+
+def un_zip(file_name):
+    zip_file = zipfile.ZipFile(file_name)
+    dir_name = os.path.splitext(file_name)[0] + "_files"
+    if os.path.exists(dir_name):
+        shutil.rmtree(dir_name)
+        os.mkdir(dir_name)
+    else:
+        os.mkdir(dir_name)
+    for names in zip_file.namelist():
+        zip_file.extract(names, dir_name)
+    zip_file.close()
+    return dir_name
+
+
+def image_turn_grey(image_file, image_type):
+    if type(image_file) == bytes:
+        image_bytes = base64.b64decode(image_file)
+        image_file = io.BytesIO(image_bytes)
+    img = Image.open(image_file)
+    img = img.convert('L')
+    img_byte_arr = io.BytesIO()
+    if image_type == 'JPG' or 'jpg':
+        image_type = 'JPEG'
+    img.save(img_byte_arr, image_type)
+    return base64.b64encode(img_byte_arr.getvalue())
+
+
+def traverse_dir(path, my_data, name):
+    files = os.listdir(path)
+    for file in files:
+        in_file = os.path.join(path, file)
+        if not os.path.isdir(in_file):
+            file_type = os.path.splitext(file)[-1][1:].upper()
+            if file_type == "ZIP":
+                in_file = un_zip(in_file)
+                traverse_dir(in_file, my_data, name + "/" + file)
+            else:
+                if file_type in all_type:
+                    my_data[0].append(image_turn_grey(in_file, file_type))
+                    my_data[1].append(file_type)
+                    my_data[2].append(name + "/" + file)
+                else:
+                    logging.warning(in_file + " does not have an image type\n")
+        else:
+            traverse_dir(in_file, my_data, name + "/" + file)
+
+
+def validate(database):
+    update_time = dt.datetime.now()
+    data = [[], [], [], [], [], update_time, database["uuid"]]
+    if database["CS"]:
+        data[4].append("CS")
+    if database["HE"]:
+        data[4].append("HE")
+    if database["LC"]:
+        data[4].append("LC")
+    if database["RV"]:
+        data[4].append("RV")
+    for index, send_image in enumerate(database["files"]):
+        file_name = database["fileNames"][index]
+        str_of_image = str(send_image)
+        binary_image = send_image[str_of_image.find(',') + 1:]
+        data_type = os.path.splitext(file_name)[-1][1:]
+        if data_type.find("zip") != -1:
+            dir_str = database["uuid"] + ".zip"
+            try:
+                image_bytes = base64.b64decode(binary_image)
+                with open(dir_str, "wb") as zip_file:
+                    zip_file.write(image_bytes)
+                    dir_name = un_zip(dir_str)
+                traverse_dir(dir_name, data, file_name)
+                shutil.rmtree(os.path.splitext(file_name)[0] + "_files")
+                os.remove(dir_str)
+            except OSError:
+                logging.error(file_name + "the uploaded file is broken")
+            except Exception as e:
+                logging.error(file_name + "the uploaded file is broken" + e)
+        else:
+            try:
+                data[0].append(image_turn_grey(binary_image, data_type))
+                data[1].append(data_type)
+                data[2].append(file_name)
+            except OSError:
+                logging.error(file_name + "the uploaded file is broken")
+            except Exception as e:
+                logging.error(file_name + "the uploaded file is broken" + e)
+    for x in range(0, len(data[0])):
+        data[3].append(x)
+    return data
+
+
+def second_validation(new_database, file_names):
+    update_time = dt.datetime.now()
+    new_data = [[], [], update_time, new_database["uuid"]]
+    if new_database["CS"]:
+        new_data[1].append("CS")
+    if new_database["HE"]:
+        new_data[1].append("HE")
+    if new_database["LC"]:
+        new_data[1].append("LC")
+    if new_database["RV"]:
+        new_data[1].append("RV")
+    for image_name in new_database["selectedFilename"]:
+        try:
+            new_data[0].append(file_names.index(image_name))
+        except ValueError:
+            logging.error(image_name + ": this image is not stored.\n")
+    return new_data
