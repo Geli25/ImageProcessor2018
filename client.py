2c43a500993d54e6924fd742bcedc754b021c5e4
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-12-10 21:41:47 -0500

    add additional module and packages for server

--- client.py
+++ client.py
@@ -5,31 +5,6 @@ import base64
 import io
 import matplotlib.image as matimage
 
-with open("test.jpg", "rb") as image_file:
-    encode = base64.b64encode(image_file.read())
-uuid1 = str(uuid.uuid4())
-p1 = {
-    "files": encode,
-    "HE": True,
-    "CS": False,
-    "LC": True,
-    "RV": False,
-    "uuid": uuid1
-}
-
-r1 = requests.post("http://127.0.0.1:5000/new_user_request", json=p1)
-print(r1.text)
-r2 = requests.get("http://127.0.0.1:5000/get_processed_result<0>".format(uuid1))
-
-
-def img_compare(img_be, img_af):
-    plt.subplot(1, 2, 1)
-    plt.imshow(decode_b64_image(img_be, 'jpeg'), interpolation='nearest', cmap='gray')
-    plt.subplot(1, 2, 2)
-    plt.imshow(decode_b64_image(img_af, 'jpeg'), cmap='gray')
-    plt.show()
-
-
 def decode_b64_image(base64_string, img_format):
     """
     decode_b64_image decode bytes string into a np array of
@@ -43,6 +18,36 @@ def decode_b64_image(base64_string, img_format):
     ft = 'JPEG' if img_format == 'jpg' or 'JPG' else img_format
     decoded_img = matimage.imread(image_buffer, format=ft)
     return decoded_img
+
+with open("test.jpg", "rb") as image_file:
+    encode = base64.b64encode(image_file.read())
+data = encode.decode('utf-8')
+print("encode:", encode)
+print("data:", data)
+print("decode:", decode_b64_image(data, 'jpg'))
+uuid1 = str(uuid.uuid4())
+p1 = {
+    "files": [data],
+    "HE": True,
+    "CS": False,
+    "LC": True,
+    "RV": False,
+    "uuid": uuid1,
+    "fileNames": ["test.jpg"]
+}
+print(uuid1)
+r1 = requests.post("http://127.0.0.1:5000/new_user_request", json=p1)
+print(r1.text)
+#r2 = requests.get("http://127.0.0.1:5000/get_processed_result{0}".format(uuid1))
+
+
+def img_compare(img_be, img_af):
+    plt.subplot(1, 2, 1)
+    plt.imshow(decode_b64_image(img_be, 'jpeg'), interpolation='nearest', cmap='gray')
+    plt.subplot(1, 2, 2)
+    plt.imshow(decode_b64_image(img_af, 'jpeg'), cmap='gray')
+    plt.show()
+
 
 
 for img_pair in r2['img_pair']:
--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
-from sqlalchemy import create_engine, Column, String, LargeBinary, DateTime, Integer, Numeric
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, String, LargeBinary, DateTime, Integer, Numeric, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 from image_processing import process_image
 import datetime
 from time import time
@@ -12,8 +12,7 @@ import matplotlib.image as matimage
 from PIL import Image
 from io import BytesIO
 from validation import validate, second_validation
-
-
+import numpy as np
 """
 File name: server.py 
 Main: this is the server.py file to build the entire image 
@@ -23,12 +22,13 @@ Date: Dec 9th 2018
 
 
 app = Flask(__name__)
-engine = create_engine("postgresql://hw188:{0}@localhost:5432/bme590finalproject".format('123456'), max_overflow=20)
+engine = create_engine("postgresql://hw188:{0}@localhost:5432/bme590finalproject".format('123456'), max_overflow=20,
+                       client_encoding='utf8')
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
-class UserRequest(Base):
+class User(Base):
     """
     UserRequest class initiate an user request instance to
     store data:
@@ -36,35 +36,87 @@ class UserRequest(Base):
         processing time, upload time, image size, actions,
         metrics
     """
-    __tablename__ = "image_processor"
+    __tablename__ = "users"
     uuid = Column(UUID, primary_key=True)
+    uploadFiles = relationship("UploadFiles")
+
+    def __init__(self, uuid):
+        self.uuid = uuid
+
+
+class UploadFiles(Base):
+    __tablename__ = "upload_files"
+    index = Column("file_index", Integer, primary_key=True)
     upload_file = Column("upload_file", LargeBinary)
     upload_file_name = Column("upload_file_name", String(32))
     upload_file_type = Column("upload_file_type", String(32))
     upload_time = Column("upload_time", DateTime)
-    processing_type = Column("image_processing_type", String(32))
-    processing_time = Column("processing_time", DateTime)
-    processed_file = Column("processed_file", LargeBinary)
-    processed_file_index = Column("processed_file_index", Integer)
-    image_size = Column("image_size (pixel):", Integer)
-    actions = Column("user_actions", Integer)
-    metrics = Column("processing_metrics", Numeric)
+    image_size = Column("image_size (pixel):", String)
+    user_uuid = Column("user_uuid", String, ForeignKey("users.uuid"))
+    processedImage = relationship("ProcessedImage")
 
-    def __init__(self, uuid, upload_file, processing_type, upload_time, file_type, processed_file_index, file_name):
-        self.uuid = uuid
+    def __int__(self, upload_file, file_type, file_name, upload_time, uuid):
         self.upload_file = upload_file
         self.upload_file_type = file_type
         self.upload_file_name = file_name
-        self.processing_type = []
+        self.image_size = str(decode_b64_image(upload_file, file_type).shape)
+        self.upload_time = upload_time
+        self.user_uuid = uuid
+
+
+class ProcessedImage(Base):
+    __tablename__ = "processed_image"
+    processing_type = Column("image_processing_type", String(32))
+    processing_time = Column("processing_time", DateTime)
+    processed_file = Column("processed_file", LargeBinary, primary_key=True)
+    processed_file_type = Column("processed_file_type", String)
+    metrics = Column("processing_metrics", Numeric)
+    image_size = Column("image_size (pixel):", Integer)
+    num_HE = Column("number_of_histogram_equalization", Integer)
+    num_CS = Column("number_of_contrast_stretching", Integer)
+    num_LC = Column("number_of_log_compression", Integer)
+    num_RV = Column("number_of_reverse_video", Integer)
+    uploadFiles_index = Column("upload_file_index", String, ForeignKey("upload_files.index"))
+
+    def __int__(self, processing_type, processing_time, processed_file, processed_file_type,
+                processing_latency, num_HE, num_CS, num_LC, num_RV, index):
+        self.processing_type = processing_type
+        self.processing_time = processing_time
+        self.processed_file = processed_file
+        self.image_size = str(decode_b64_image(processed_file, processed_file_type).shape)
+        self.metrics = processing_latency
+        self.num_HE = num_HE
+        self.num_CS = num_CS
+        self.num_LC = num_LC
+        self.num_RV = num_RV
+        self.uploadFiles_index = index
+
+
+Base.metadata.create_all(engine)
+
+
+class HandleNewUserRequest(object):
+    """
+    this class functions as a handle to process user request, for each new
+    request, including new user and update, will initiate an instance of this class
+    to handle processing task
+    """
+
+    def __init__(self, uuid, upload_file, processing_type, upload_time, file_type,
+                 processed_file_index, file_name):
+        self.uuid = uuid
+        self.upload_file = upload_file
+        self.upload_file_type = file_type
+        self.processed_file_index = processed_file_index
+        self.upload_file_name = file_name
+        self.upload_time = upload_time
+        self.processing_type = processing_type
         self.processing_time = []
         self.processed_file = []
-        self.upload_time = []
+        self.image_size_original = []
+        self.image_size_processed = []
         self.actions = [0, 0, 0, 0]  # [num_hist_eq, num_contr_stre, num_log_com, num_reverse_video]
         self.metrics = []  # processing latency
-        self.image_size = []
-        self.processed_file_index = processed_file_index
-        self.upload_time.append(upload_time)
-        self.processing_type.append(processing_type)
 
     def image_processing(self):
         """
@@ -75,15 +127,13 @@ class UserRequest(Base):
         for index in self.processed_file_index:
             time_be = time()
             current_img = self.upload_file[index]
-            print('file_type:', self.upload_file_type[index], 'processing_type:', self.processing_type[-1], 'name:', self.upload_file_name[index])
             decode_img = decode_b64_image(current_img, self.upload_file_type[index])
-            decode_img = decode_img[:, :, 0] if decode_img.shape[2] == 3 else decode_img
             out_img, actions, size = process_image(decode_img, self.processing_type[-1], self.actions)
             time_af = time()
             self.metrics = time_af - time_be
             print(out_img.shape)
             processed.append(encode_nparray_to_img(out_img, self.upload_file_type[index]))
-            self.image_size.append(size)
+            self.image_size_processed.append(size)
             self.actions = actions
         self.processed_file.append(processed)
         self.processing_time = datetime.datetime.now() - self.upload_time[-1]
@@ -94,29 +144,34 @@ class UserRequest(Base):
         to the front end
         """
         img_pair = []
+        his_pair = []
         print(len(self.processed_file))
         for index in self.processed_file_index:
-            img_pair.append([self.upload_file[index], self.processed_file[-1][index]])
+            decode = decode_b64_image(self.processed_file[-1][index], self.upload_file_type[index])
+            # decode_his = np.histogram(decode, bins=254)
+            # upload_decode = decode_b64_image(self.upload_file[index], self.file_type[index])
+            # decode_his2 = np.histogram(upload_decode, bins=254)
+            # his_pair.append([decode_his2, decode_his])
+            if self.upload_file_type[index] == "JPEG" or "JPG":
+                format1 = encode_nparray_to_img(decode, "PNG")
+                format2 = encode_nparray_to_img(decode, "TIFF")
+                img_pair.append([self.upload_file[index], self.processed_file[-1][index], format2, format1])
+            elif self.upload_file_type[index] == "PNG":
+                format1 = encode_nparray_to_img(decode, "JPEG")
+                format2 = encode_nparray_to_img(decode, "TIFF")
+                img_pair.append([self.upload_file[index], format1, format2, self.processed_file[-1][index]])
+            else:
+                format1 = encode_nparray_to_img(decode, "PNG")
+                format2 = encode_nparray_to_img(decode, "JPEG")
+                img_pair.append([self.upload_file[index], format2, self.processed_file[-1][index]], format1)
+
         return {"uuid": self.uuid,
                 "img_pair": img_pair,
-                "img_size": self.image_size,
+                "histogram_pair": his_pair,
+                "img_size": self.image_size_processed,
                 "processed_time": self.processing_time,
-                "file_name": self.upload_file_name
+                "fileNames": self.upload_file_name
                 }
-
-    def update_processing_file(self, processing_type, upload_time, file_index):
-        """
-        update_processing_file updates the
-        :param processing_type:
-        :param upload_time:
-        :param file_index:
-        """
-        self.processing_type.append(processing_type)
-        self.upload_time.append(upload_time)
-        self.processed_file_index = file_index
-
-
-Base.metadata.create_all(engine)
 
 
 def encode_nparray_to_img(np_array, img_format):
@@ -132,7 +187,7 @@ def encode_nparray_to_img(np_array, img_format):
     im2 = image.convert("L")
     ft = 'JPEG' if img_format == 'jpg' or 'JPG' else img_format
     im2.save(buffer, format=ft)
-    return base64.b64encode(buffer.getvalue())
+    return base64.b64encode(buffer.getvalue()).decode('utf-8')
 
 
 def decode_b64_image(base64_string, img_format):
@@ -158,16 +213,26 @@ def initial_new_image_processing():
     """
     r = request.get_json()
     data = validate(r)
+    user_request = HandleNewUserRequest(data[6], data[0], data[4], data[5], data[1], data[3], data[2])
+    user_request.image_processing()
     session = Session()
-    user = UserRequest(data[6], data[0], data[4], data[5], data[1], data[3], data[2])
-    user.image_processing()
+    user = User(user_request.uuid)
+    for index in data[3]:
+        files = UploadFiles(user_request.upload_file[index], user_request.upload_file_type[index],
+                            user_request.upload_file_name[index], user_request.upload_time, user_request.uuid)
+        processed_files = ProcessedImage(user_request.processing_type, user_request.processing_time[index],
+                                         user_request.processed_file[index], user_request.upload_file_type[index],
+                                         user_request.metrics[index], user_request.actions[0], user_request.actions[1],
+                                         user_request.actions[2], user_request.actions[3], index)
+        files.processedImage.append(processed_files)
+        user.uploadFiles.append(files)
     session.add(user)
     session.commit()
-    session.close()
     if not data[7]:
         result = {"message": "Successfully added and processed user data"}
     else:
         result = {data[7]}
+    session.close()
     return jsonify(result)
 
 
@@ -176,26 +241,26 @@ def add_new_processing_to_exist_user():
     r = request.get_json()
     data = second_validation(r)
     session = Session()
-    user = session.query(UserRequest).filter_by(UUID=data[3])
-    user.update_processing(data[1], data[2], data[0])
-    user.image_processing()
-    session.commit()
-    session.close()
+    update = (Session.query(User, UploadFiles, ProcessedImage).filter(User.uuid == UploadFiles.user_uuid)
+              .filter(UploadFiles.index == ProcessedImage.uploadFiles_index).filter(User.uuid == data[3]).all())
+
     if not data[4]:
         result = {"message": "Successfully added and processed user data"}
     else:
         result = {data[4]}
+    session.commit()
+    session.close()
     return jsonify(result)
 
 
 @app.route("/get_processed_result<uuid>", methods=['GET'])
 def get_processed_result(uuid):
     session = Session()
-    user = session.query(UserRequest).filter_by(UUID=uuid)
-    result = user.to_ui()
-    session.commit()
-    session.close()
-    return jsonify(result)
+    info = (Session.query(User, UploadFiles, ProcessedImage).filter(User.uuid == UploadFiles.user_uuid)
+            .filter(UploadFiles.index == ProcessedImage.uploadFiles_index).filter(User.uuid == uuid).all())
+    print(info)
+
+    return
 
 
 if __name__ == '__main__':
