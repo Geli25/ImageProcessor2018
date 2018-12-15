3f8a8c6bbbf1dedab704aecaa55ba6d7d4d395f0
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-12-14 23:04:57 -0500

    fix local unbound error

--- server.py
+++ server.py
@@ -433,6 +433,8 @@ def add_new_processing_to_exist_user():
             total_gc.append(row.num_GC)
             index_of_underscore = row.processed_file_name.rfind("_")
             number_after = int(row.processed_file_name[index_of_underscore+1:])
+            pre_last_processed_file_name = \
+                row.processed_file_name[:index_of_underscore]
             if number_after > number_max:
                 number_max = number_after
                 pre_last_processed_file_name = \
